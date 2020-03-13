f1e8f83a1b2be2f03d597205191bf425759258c8
Katie Layne <klayne1129@gmail.com>
2020-03-12 23:39:45 -0500

    H2 Memory Database connected

--- src/api/recipes/RecipeDataService.js
+++ src/api/recipes/RecipeDataService.js
@@ -1,35 +1,36 @@
 import Axios from "axios"
-import {API_URL} from '../../Constants.js'
+import {API_URL, JPA_API_URL} from '../../Constants.js'
+
 
 class RecipeDataService {
     //passing name as a parameter and removing the hardcode makes it customizable for users
     retrieveAllRecipes(name) {
         // console.log('executed service')
         //tell promise to get desired url we created in java and eclipse
-        return Axios.get(`${API_URL}/users/${name}/recipes`)   
+        return Axios.get(`${JPA_API_URL}/users/${name}/recipes`)   
     }
 
     retrieveRecipe(name, id) {
         
-        return Axios.get(`${API_URL}/users/${name}/recipes/${id}`)   
+        return Axios.get(`${JPA_API_URL}/users/${name}/recipes/${id}`)   
     }
 
     deleteRecipe(name, id) {
         // console.log('executed service')
         //tell promise to get desired url we created in java and eclipse
-        return Axios.delete(`${API_URL}/users/${name}/recipes/${id}`)   
+        return Axios.delete(`${JPA_API_URL}/users/${name}/recipes/${id}`)   
     }
 
     updateRecipe(name, id, recipe) {
         // console.log('executed service')
         //tell promise to get desired url we created in java and eclipse
-        return Axios.put(`${API_URL}/users/${name}/recipes/${id}`, recipe)   
+        return Axios.put(`${JPA_API_URL}/users/${name}/recipes/${id}`, recipe)   
     }
 
     createRecipe(name, recipe) {
         // console.log('executed service')
         //tell promise to get desired url we created in java and eclipse
-        return Axios.post(`${API_URL}/users/${name}/recipes`, recipe)   
+        return Axios.post(`${JPA_API_URL}/users/${name}/recipes`, recipe)   
     }
 
 }
